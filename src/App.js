d84cbd8625908ca5c0fe2879aa80b0c6aaf3a96c
NguyenUyen0904 <nguyen.uyen.0904@gmail.com>
2021-04-12 17:26:15 +0000

    Add sidebar to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [animeList, SetAnimeList] = useState([]);
+	const [topAnime, SetTopAnime] = useState([]);
+	const [search, SetSearch] = useState("");
+	const GetTopAnime = async () => {
+		const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+			.then(res => res.json());
+
+		SetTopAnime(temp.top.slice(0, 5));
+	}
+	useEffect(() => {
+		GetTopAnime();
+	}, []);
   return (
     <div className="App">
+      
+      <div className="content-wrap">
+				<Sidebar 
+					topAnime={topAnime} />
+				
+			</div>
 
     </div>
   );
